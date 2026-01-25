e21a535d399b2f4c2ff214c9f9cf2ada28a666c1
manjaro <julcia.aftyka@gmail.com>
2026-01-25 22:53:51 +0100

    fixed PolymorphismTest.java

--- src/test/java/com/stockmarket/domain/PolymorphismTest.java
+++ src/test/java/com/stockmarket/domain/PolymorphismTest.java
@@ -1,36 +1,40 @@
 package com.stockmarket.domain;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PolymorphismTest {
 
     @Test
-    void testShareRealValueIsNotTheSameAsCommodityRealValue() {
-        Asset asset1 = new Share("ABC", "Some share", 200);
-        Asset asset2 = new Commodity("ABC", "Some commodity", 200);
-        assertNotEquals(asset1.calculateRealValue(5), asset2.calculateRealValue(5), 0.0001);
+    void testDifferentLotValueImplementations1() {
+        PurchaseLot lot = new PurchaseLot(LocalDate.now(), 5, 100);
+
+        Asset share = new Share("ABC", "Share", 200);
+        Asset commodity = new Commodity("ABC", "Commodity", 200);
+        Asset currency = new Currency("ABC", "Currency", 200, 40);
+
+        double shareValue = share.calculateLotValue(lot);
+        double commodityValue = commodity.calculateLotValue(lot);
+        double currencyValue = currency.calculateLotValue(lot);
+
+        assertNotEquals(shareValue, currencyValue, 0.0001);
     }
 
     @Test
-    void testShareRealValueIsNotTheSameAsCurrencyRealValue() {
-        Asset asset1 = new Share("ABC", "Some share", 200);
-        Asset asset2 = new Currency("ABC", "Some currency", 200, 40);
-        assertNotEquals(asset1.calculateRealValue(5), asset2.calculateRealValue(5), 0.0001);
-    }
+    void testDifferentLotValueImplementations2() {
+        PurchaseLot lot = new PurchaseLot(LocalDate.now(), 5, 100);
 
-    @Test
-    void testCurrencyRealValueIsNotTheSameAsCommodityRealValue() {
-        Asset asset1 = new Currency("ABC", "Some currency", 200, 40);
-        Asset asset2 = new Commodity("ABC", "Some currency", 200);
-        assertNotEquals(asset1.calculateRealValue(5), asset2.calculateRealValue(5), 0.0001);
-    }
+        Asset share = new Share("ABC", "Share", 200);
+        Asset commodity = new Commodity("ABC", "Commodity", 200);
+        Asset currency = new Currency("ABC", "Currency", 200, 40);
 
-    @Test
-    void testAll3HaveDifferentRealValues() {
-        Asset asset1 = new Share("ABC", "Some share", 200);
-        Asset asset2 = new Currency("ABC", "Some currency", 200, 40);
-        Asset asset3 = new Commodity("ABC", "Some commmodity", 200);
-        assertFalse(asset1.equals(asset2) && asset2.equals(asset3));
+        double shareValue = share.calculateLotValue(lot);
+        double commodityValue = commodity.calculateLotValue(lot);
+        double currencyValue = currency.calculateLotValue(lot);
+
+        assertNotEquals(commodityValue, currencyValue, 0.0001);
     }
 }
